1842d1c0dcad3605566a44c95fecfb3b34288ae8
Evan <evan@ethicalhack.ing>
2025-02-10 00:42:45 -0500

    modify data import, move steps for decluttering, and add scylladb setup docker compose file

--- data_import/create-data.py
+++ data_import/create-data.py
@@ -3,21 +3,32 @@ import multiprocessing
 import os
 import hashlib
 
-DEBUG = True
+DEBUG = False
 HASH_SEED = b"ABC"
 BATCH_SIZE = 10_000
 AUTO_REMOVE = True
+DIR_NAME = "csvs"
+
+def create_dir():
+    debug_print(f"Trying to create directory {DIR_NAME} ...")
+    try:
+        os.mkdir(DIR_NAME)
+        print(f"Directory '{DIR_NAME}' created successfully.")
+    except FileExistsError:
+        print(f"Directory '{DIR_NAME}' already exists.")
+    except PermissionError:
+        print(f"Permission denied: Unable to create '{DIR_NAME}'.")
 
 def main(): 
     num_rows_options = [1_000, 10_000, 100_000, 1_000_000, 10_000_000, 100_000_000]
-
+    create_dir()
     with multiprocessing.Pool() as pool: 
         pool.map(populate_csv, num_rows_options)
 
 
 def populate_csv(num_rows): 
     data = []
-    file_name = f"{num_rows}_rows.csv"
+    file_name = os.path.join(DIR_NAME, f"{num_rows}_rows.csv")
     debug_print(f"Populating csv {file_name}")
     
     if AUTO_REMOVE:
@@ -33,7 +44,7 @@ def populate_csv(num_rows):
         num_loops = 1
         if BATCH_SIZE < num_rows:
             num_loops = num_rows // BATCH_SIZE
-        debug_print(f"Using {num_loops} loops for batch size {BATCH_SIZE}")
+        debug_print(f"Using {num_loops} loops for batch size {BATCH_SIZE} for {num_rows} rows")
         running_total = 0
         for _ in range(num_loops):
             data = []
